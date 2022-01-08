96b2d0bdf79d8202e6d1cb9d542ed10969cde67c
chhornchanpiseth <85533571+chhornchanpiseth@users.noreply.github.com>
2022-01-08 21:55:08 +0700

    Add files via upload

---
+++ CRUD/script.js
@@ -0,0 +1,73 @@
+var selectedRow = null
+
+function onFormSubmit(){
+    var formData = readFormDate();
+    if(selectedRow == null)
+
+        insertNewRecord(formData);
+        else
+        updateRecord(formData);
+
+    resetForm();
+}
+
+function readFormDate(){
+    var formData = {};
+    formData["fullName"] = document.getElementById("fullName").value;
+    formData["countryCode"] = document.getElementById("countryCode").value;
+    formData["country"] = document.getElementById("country").value;
+    formData["city"] = document.getElementById("city").value;
+    return formData;
+}
+
+function insertNewRecord(data) {
+    var table = document.getElementById("tourist").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML= data.fullName;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML= data.countryCode;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML= data.country;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML= data.city;
+
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML= `<a onclick = "onEdit(this)">Edit</a>
+                      <a onclick = "onDelete(this)">Delete</a>`;
+
+}
+
+function resetForm() {
+    document.getElementById("fullname").value="";
+    document.getElementById("countrycode").value="";
+    document.getElementById("country").value="";
+    document.getElementById("city").value="";
+}
+
+// Edit and update 
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("fullName").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("countryCode").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("country").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("city").value = selectedRow.cells[3].innerHTML;
+
+}
+function updateRecord(formData){
+    selectedRow.cells[0].innerHTML = formData.fullName;
+    selectedRow.cells[1].innerHTML = formData.countryCode;
+    selectedRow.cells[2].innerHTML = formData.country;
+    selectedRow.cells[3].innerHTML = formData.city;
+   
+}
+
+function onDelete(td){
+    if(confirm("Are you sure to delete this record ?????")) {
+        row = td.parentElement.parentElement;
+        document.getElementById("tourist").deleteRow(row.rowIndex);
+        resetForm();
+    }
+   
+}
+
